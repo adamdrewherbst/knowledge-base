f2d88d621e0f5d524b232f50f8a75d6b869ada14
Adam Herbst <adamdrewherbst@gmail.com>
2018-07-11 19:59:00 +0000

    numeric values for nodes

--- controllers/default.py
+++ controllers/default.py
@@ -38,99 +38,8 @@ def concept():
             'framework': [lambda row: A('Use', _href='javascript:relation.useFramework('+str(row.id)+')')],
             'law': [lambda row: A('Use', _href='javascript:relation.useLaw('+str(row.id)+')')]
         },
-        linked_tables=[db.framework_dependency.framework, 'concept', 'law'])
+        linked_tables=[db.framework_dependency.framework, 'concept', 'law', db.concept_dependency.concept])
     return locals()
-
-
-def useFrameworkOrLaw():
-    currentFramework = int(request.vars.currentFramework or -1)
-    currentLaw = int(request.vars.currentLaw or -1)
-    recordType = request.vars.type
-    recordId = int(request.vars.id)
-
-    ret = {'frameworks': [], 'law': {'nodes': {}, 'predicates': {}}}
-
-    print('{timestamp} -- using framework').format(timestamp=datetime.utcnow().isoformat())
-
-    if (recordType == 'framework' and recordId == currentFramework) \
-        or (recordType == 'law' and recordId == currentLaw):
-            ret['message'] = "Framework %d already in use" % recordId
-            return response.json(ret)
-
-    if recordType == 'framework':
-        newFramework = recordId
-    elif recordType == 'law' and recordId != currentLaw:
-        rows = db(db.law.id == recordId).select()
-        law = rows[0]
-        ret['law']['id'] = law.id
-        ret['law']['name'] = law.name
-        ret['law']['description'] = law.description
-        ret['law']['framework'] = law.framework
-        for node in db(db.node.law == recordId).iterselect():
-            ret['law']['nodes'][node.id] = \
-                {'id': node.id, 'law': recordId, 'concept': node.concept, 'head': node.head, 'reference': node.reference}
-            for predicate in db(db.predicate.node == node.id).iterselect():
-                if predicate.predicate_group not in ret['law']['predicates']:
-                    ret['law']['predicates'][predicate.predicate_group] = {}
-                ret['law']['predicates'][predicate.predicate_group][node.id] = True   #not clear how to avoid repeating this command
-        newFramework = law.framework
-
-    #collect the concepts corresponding to the new framework and all its dependencies
-    if newFramework != currentFramework:
-        frameworks = [newFramework]
-        while frameworks:
-            fid = frameworks.pop(0)
-            rows = db(db.framework.id == fid).select()
-            framework = rows[0]
-            ret['frameworks'].append(
-                {'id': fid, 'name': framework.name, 'description': framework.description, 'concepts': []});
-            for concept in db(db.concept.framework == fid).iterselect():
-                ret['frameworks'][-1]['concepts'].append(
-                    {'id': concept.id, 'name': concept.name, 'description': concept.description, 'framework': fid});
-            for dep in db(db.framework_dependency.framework == fid).iterselect(db.framework_dependency.dependency):
-                frameworks.append(dep.dependency)
-
-    print('{timestamp} -- done using framework').format(timestamp=datetime.utcnow().isoformat())
-
-    return response.json(ret)
-
-
-def saveLaw():
-    import json, urllib
-    body = request.body.read()
-    body = urllib.unquote(body).decode('utf8')
-
-    print('{timestamp} -- saving law').format(timestamp=datetime.utcnow().isoformat())
-    print('BODY: {body}').format(body=body)
-
-    request_vars = json.loads(body)
-
-    lawId = int(request_vars['id'])
-    nodes = request_vars['nodes']
-    predicates = request_vars['predicates']
-    ret = {'success': False}
-
-    idMap = {None: None}
-    allNodes = {}
-    for node in nodes:
-        #data = {'law': lawId, 'concept': node['concept'], 'predicate': node['predicate']}
-        nodeId = db.node.update_or_insert(db.node.id == node['id'],
-            law=node['law'], concept=node['concept'])
-        idMap[node['id']] = nodeId
-        allNodes[nodeId] = True
-
-    for node in db(db.node.law == lawId).iterselect():
-        db(db.predicate.node == node.id).delete()
-        if node.id not in allNodes:
-            node.delete()
-
-    for node in nodes:
-        db(db.node.id == idMap[node['id']]).update(head = idMap[node['head']], reference = idMap[node['reference']])
-    for predicate in predicates:
-        db.predicate.insert(node=idMap[predicate['node']], predicate_group=predicate['predicate_group'])
-
-    ret['success'] = True
-    return response.json(ret)
 
 
 def initRelation():
@@ -145,6 +54,11 @@ def initRelation():
 
     ret = {'myNodes': [], 'concepts': {}, 'laws': {}, 'nodes': {}, 'predicates': {}, 'nextNodeId': -1}
 
+    #include all concepts not specific to any framework (ROOT and Anything)
+    for concept in db(db.concept.framework == None).iterselect():
+        ret['concepts'][concept.id] = \
+            {'id': concept.id, 'name': concept.name, 'description': concept.description, 'framework': concept.framework, 'symmetric': concept.symmetric or False};
+
     frameworks = []
     if frameworkId > 0:
         frameworks.append(frameworkId)
@@ -155,12 +69,15 @@ def initRelation():
         fid = frameworks.pop(0)
         for concept in db(db.concept.framework == fid).iterselect():
             ret['concepts'][concept.id] = \
-                {'id': concept.id, 'name': concept.name, 'description': concept.description, 'framework': fid, 'symmetric': concept.symmetric or False};
+                {'id': concept.id, 'name': concept.name, 'description': concept.description, 'framework': fid, \
+                'symmetric': concept.symmetric or False, 'dependencies': {}};
+            for dep in db(db.concept_dependency.concept == concept.id).iterselect():
+                ret['concepts'][concept.id]['dependencies'][dep.id] = True
         for law in db(db.law.framework == fid).iterselect():
             ret['laws'][law.id] = {'id': law.id, 'name': law.name, 'description': law.description, 'nodes': [], 'predicates': {}, 'notDeepNode': {}};
             for node in db(db.node.law == law.id).iterselect():
                 ret['nodes'][node.id] = \
-                    {'id': node.id, 'law': node.law, 'concept': node.concept, 'head': node.head, 'reference': node.reference};
+                    {'id': node.id, 'law': node.law, 'concept': node.concept, 'head': node.head, 'reference': node.reference, 'values': node.node_values};
                 ret['laws'][law.id]['nodes'].append(node.id)
                 for predicate in db(db.predicate.node == node.id).iterselect():
                     if node.concept not in ret['predicates']:
@@ -180,6 +97,44 @@ def initRelation():
 
     rows = db.executesql("select `auto_increment` from information_schema.tables where table_name = 'node'")
     ret['nextNodeId'] = rows[0][0]
+
+    ret['success'] = True
+    return response.json(ret)
+
+
+def saveRelation():
+    import json, urllib
+    body = request.body.read()
+    body = urllib.unquote(body).decode('utf8')
+
+    print('{timestamp} -- saving law').format(timestamp=datetime.utcnow().isoformat())
+    print('BODY: {body}').format(body=body)
+
+    request_vars = json.loads(body)
+
+    lawId = int(request_vars['id'])
+    nodes = request_vars['nodes']
+    predicates = request_vars['predicates']
+    ret = {'success': False}
+
+    idMap = {None: None}
+    allNodes = {}
+    for node in nodes:
+        #data = {'law': lawId, 'concept': node['concept'], 'predicate': node['predicate']}
+        nodeId = db.node.update_or_insert(db.node.id == node['id'],
+            law=node['law'], concept=node['concept'], node_values=node['values'])
+        idMap[node['id']] = nodeId
+        allNodes[nodeId] = True
+
+    for node in db(db.node.law == lawId).iterselect():
+        db(db.predicate.node == node.id).delete()
+        if node.id not in allNodes:
+            node.delete()
+
+    for node in nodes:
+        db(db.node.id == idMap[node['id']]).update(head = idMap[node['head']], reference = idMap[node['reference']])
+    for predicate in predicates:
+        db.predicate.insert(node=idMap[predicate['node']], predicate_group=predicate['predicate_group'])
 
     ret['success'] = True
     return response.json(ret)
