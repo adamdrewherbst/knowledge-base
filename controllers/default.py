e417c784dad6b13c78e35944323a5e328ae94e5a
Adam Herbst <adamdrewherbst@gmail.com>
2018-07-12 00:03:49 +0000

    almost working

--- controllers/default.py
+++ controllers/default.py
@@ -123,8 +123,12 @@ def saveRelation():
         #data = {'law': lawId, 'concept': node['concept'], 'predicate': node['predicate']}
         nodeId = db.node.update_or_insert(db.node.id == node['id'],
             law=node['law'], concept=node['concept'], node_values=node['values'])
+        if nodeId is None:
+            nodeId = node['id']
         idMap[node['id']] = nodeId
         allNodes[nodeId] = True
+
+    print('Keeping nodes: {allNodes}').format(allNodes=allNodes)
 
     for node in db(db.node.law == lawId).iterselect():
         db(db.predicate.node == node.id).delete()
